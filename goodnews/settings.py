54cf4d5a82f4eadd32c1a028063194a6e36e4091
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-09-02 16:13:10 +0000

    add debug settings to true and change approved field to status

--- goodnews/settings.py
+++ goodnews/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-fernvr-goodnewsblog-wvehqd4lm73.ws.codeinstitute-ide.net', '.herokuapp.com']
 
@@ -79,11 +79,12 @@ WSGI_APPLICATION = 'goodnews.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # DATABASES = {
-    #'default': {
-       # 'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#  }
+
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
