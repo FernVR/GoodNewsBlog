c6ca5e7f7ffa5331cb06eb513d3c1a1ee0b23c2d
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-09-02 14:09:38 +0000

    error

--- goodnews/settings.py
+++ goodnews/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-io^@4)7*(i2dqwl0pv5x4m)*e#lshy)7kfom(1#-992skokz34'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
