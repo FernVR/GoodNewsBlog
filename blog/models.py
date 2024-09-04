8e2d4d4d7f15c2e912e7952b0a6e34edc2acef50
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-09-04 11:05:35 +0000

    add meta class and method to post and comment models

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,12 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
 
 class Comment(models.Model):
     author = models.ForeignKey(
@@ -28,5 +34,11 @@ class Comment(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment: ' {self.body} ' by {self.author}"
 
 
