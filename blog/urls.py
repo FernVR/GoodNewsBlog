46fdd4c768ab2381f1ccf2ca87f853ca1330ff08
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-09-30 07:11:36 +0000

    add order to url paths to fix deploy issue

--- blog/urls.py
+++ blog/urls.py
@@ -3,16 +3,16 @@ from django.urls import path
 from .views import like_post, dislike_post
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path('<slug:slug>/edit_comment/<int:comment_id>',
-         views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/delete_comment/<int:comment_id>',
-         views.comment_delete, name='comment_delete'),
     path('profile', views.user_profile, name='user_profile'),
     path('profile/update/', views.profile_update_view, name='profile-update'),
     path('profile/delete/', views.profile_delete_view, name='profile-delete'),
-    path('post/<int:post_id>/like/', like_post, name='like_post'),
-    path('post/<int:post_id>/dislike/', dislike_post, name='dislike_post'),
-
+    
+    path('post/', views.PostList.as_view(), name='home'),
+    path('post/<slug:slug>/', views.post_detail, name='post_detail'),
+    path('post/<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('post/<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
+    path('post/<slug:slug>/like/', like_post, name='like_post'),
+    path('post/<slug:slug>/dislike/', dislike_post, name='dislike_post'),
 ]
