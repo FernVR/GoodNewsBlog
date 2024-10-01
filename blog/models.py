469d39948cb74ee063c9789fabcac5588ddebac3
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-10-01 13:21:11 +0000

    add signals to models file and create new profile when new user is created

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.utils.text import slugify
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -69,3 +71,14 @@ class Profile(models.Model):
     location = models.CharField(max_length=250, blank=True)
     def __str__(self):
         return self.user.username
+
+
+# Signal to create a profile when a new user is created
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.profile.save()
