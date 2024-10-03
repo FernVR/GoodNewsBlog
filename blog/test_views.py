af37ed5980e6790332e398f62c3e93c18f00e111
Fern Van Rooyen <fern.w.1998@gmail.com>
2024-10-03 14:44:31 +0000

    add test user profile view

--- blog/test_views.py
+++ blog/test_views.py
@@ -21,7 +21,6 @@ class TestBlogViews(TestCase):
             content="Blog content",
             status=1
         )
-
     
     def test_post_detail_view(self):
         response = self.client.get(reverse('post_detail', args=['blog-title']))
@@ -30,3 +29,53 @@ class TestBlogViews(TestCase):
         self.assertContains(response, "Blog title")
         self.assertContains(response, "Blog content")
         self.assertIsInstance(response.context['comment_form'], CommentForm)
+    
+    def test_successful_comment_submission(self):
+        self.client.login(username="myUsername", password="myPassword")
+        post_data = {
+            'body': 'This is a test comment.'
+        }
+        response = self.client.post(reverse('post_detail', args=['blog-title']), post_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Comment submitted and awaiting approval')
+    
+    def test_comment_edit(self):
+        comment = Comment.objects.create(
+            post=self.post, author=self.user, body='Original Comment'
+        )
+        self.client.login(username="myUsername", password="myPassword")
+        response = self.client.post(reverse('comment_edit', args=['blog-title', comment.id]), {
+            'body': 'Updated Comment'
+        })
+        self.assertRedirects(response, reverse('post_detail', args=['blog-title']))
+        comment.refresh_from_db()
+        self.assertEqual(comment.body, 'Updated Comment')
+    
+    def test_comment_delete(self):
+        comment = Comment.objects.create(
+            post=self.post, author=self.user, body='Comment to be deleted'
+        )
+        self.client.login(username="myUsername", password="myPassword")
+        response = self.client.post(reverse('comment_delete', args=['blog-title', comment.id]))
+        self.assertRedirects(response, reverse('post_detail', args=['blog-title']))
+        self.assertFalse(Comment.objects.filter(id=comment.id).exists())
+
+    def test_user_profile_view(self):
+        self.client.login(username="myUsername", password="myPassword")
+    
+        # Simulate submitting a post via POST request
+        post_data = {
+            'title': 'Test Post',
+            'excerpt': 'Test excerpt',
+            'content': 'This is the content of the test post.',
+            'featured_image': '',  # Optional field, can leave empty for this test
+        }
+    
+        response = self.client.post(reverse('user_profile'), post_data)
+    
+        # Check if the response contains the success message after submission
+        self.assertEqual(response.status_code, 302)  # Expecting a redirect after successful post submission
+    
+        # Follow the redirect and check if the success message is present
+        follow_response = self.client.get(reverse('user_profile'))
+        self.assertContains(follow_response, 'Your post has been submitted and is awaiting approval.')
